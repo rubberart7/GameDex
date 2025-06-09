c504bc9bef1ec6f97fe931ccf6ecee5cbdbe16c9
mbyaf <rubberart7@users.noreply.github.com>
2025-06-09 18:08:17 -0400

    completed login form

---
+++ frontend/app/components/ui/InputComp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputComp = () => {
+  return (
+    <input type="text" 
+    className='flex rounded-md shadow-xs border-gray-600 bg-blue-950 text-gray-200 focus:border-gray-200 px-4'
+    placeholder='John Doe'
+    />
+  )
+}
+
+export default InputComp
