fea2775a608a782d7a30c82850eaca1d8343ec96
mbyaf <rubberart7@users.noreply.github.com>
2025-06-28 23:09:16 -0400

    authenticated login allows you view a page

--- frontend/app/new/page.tsx
+++ frontend/app/new/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import RequireAuth from '../components/RequireAuth'
+import HomeNavBar from '../components/ui/HomeNavBar'
 
 const TestingPage = () => {
   return (
     <RequireAuth>
-      <div>
-        <h1>You are logged in and can view this page!</h1>
-      </div>
+      <main className="min-h-screen flex flex-col">
+      <HomeNavBar />
+
+      <section className="flex-grow bg-gray-950 text-gray-100 flex flex-col items-center justify-center px-4">
+        <div className="max-w-3xl text-center space-y-6">
+          <h1 className="text-4xl sm:text-5xl font-bold tracking-tight text-slate-100">
+            Congratulations!          </h1>
+
+          <p className="text-blue-400 text-lg">
+            You are logged in and can view this page!
+          </p>
+        </div>
+      </section>
+    </main>
     </RequireAuth>
     
   )
