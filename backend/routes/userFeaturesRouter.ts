2b39d0f41e955c24661b08ed1c554f4ec5bc8dda
mbyaf <rubberart7@users.noreply.github.com>
2025-07-19 20:22:11 -0400

    implemented code for adding to wishlist and setup routes for it

--- backend/routes/userFeaturesRouter.ts
+++ backend/routes/userFeaturesRouter.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middleware/verifyJWT';
 import { addToLibrary } from '../controllers/addToLibraryController';
+import { addToWishlist } from '../controllers/addToWishListController';
 
 const router = Router();
 
 router.post('/library', verifyJWT, addToLibrary);
+router.post('/wishlist', verifyJWT, addToWishlist);
+// the next that executes is this addToLibrary function, the verifyJWT is the middleware
 
 export default router;
